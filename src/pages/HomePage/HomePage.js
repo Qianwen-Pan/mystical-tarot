fc6a13abb8f677b973c92ef98d791d1f70dff7c4
qianwen_pan <qianwen.pan2021@gmail.com>
2024-06-05 11:12:34 -0400

    add comming soon page

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -2,13 +2,13 @@ import React, { useState } from "react";
 import CardDeck from "../../components/CardDeck/CardDeck";
 import "./HomePage.scss";
 import decisionMakingImg from "../../assets/images/glod_black_front_back.png";
-import dreamCardImg from "../../assets/images/gold_tarot_fb.png";
+import dreamCardImg from "../../assets/images/image-removebg.png";
 
 const HomePage = () => {
   return (
     <div className="home">
-      <img className="home__cardimage" src={decisionMakingImg} alt=""></img>
       <img className="home__cardimage" src={dreamCardImg} alt=""></img>
+      <img className="home__cardimage" src={decisionMakingImg} alt=""></img>
     </div>
   );
 };
