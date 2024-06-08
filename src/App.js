7c6b04c5d697c42322d32c92fca08a0cced6ae92
qianwen_pan <qianwen.pan2021@gmail.com>
2024-06-07 21:13:07 -0400

    Update game page to add animation of shuffle and spread card

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import CommingSoon from "./pages/CommingSoon/CommingSoon";
+import Game from "./pages/Game/Game";
 
 const App = () => {
   return (
@@ -11,8 +13,10 @@ const App = () => {
       <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/game" element={<Game />} />
+        <Route path="*" element={<CommingSoon />} />
       </Routes>
-
+      {/* <Footer /> */}
     </BrowserRouter>
   );
 };
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -4,11 +4,33 @@ import "./HomePage.scss";
 import decisionMakingImg from "../../assets/images/glod_black_front_back.png";
 import dreamCardImg from "../../assets/images/image-removebg.png";
 
+import theFool from "../../assets/images/aurora_cards/the_fool.png";
+import theEmperor from "../../assets/images/aurora_cards/the_emperor.png";
+import theEmpress from "../../assets/images/aurora_cards/the_empress.png";
+import theHighPrestess from "../../assets/images/aurora_cards/the_high_priestess.png";
+import theMagician from "../../assets/images/aurora_cards/the_magician.png";
+import CardAnimation from "../../components/CardAnimation/CardAnimation";
+
 const HomePage = () => {
+  const images = [
+    theFool,
+    theEmperor,
+    theEmpress,
+    theHighPrestess,
+    theMagician,
+  ];
   return (
     <div className="home">
-      <img className="home__cardimage" src={dreamCardImg} alt=""></img>
-      <img className="home__cardimage" src={decisionMakingImg} alt=""></img>
+      <div className="home__hero">
+        <div className="home__buttons">
+          <button className="home__button">Decision Making</button>
+          <button className="home__button">Dream Interpretaion</button>
+        </div>
+
+        <CardAnimation images={images} />
+      </div>
+      {/* <img className="home__cardimage" src={dreamCardImg} alt=""></img>
+      <img className="home__cardimage" src={decisionMakingImg} alt=""></img> */}
     </div>
   );
 };
